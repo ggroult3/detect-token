82bc7debe90b32da5404d304c95ff2ef815f7574
Guillaume Groult <guillaume.groult3@orange.fr>
2020-03-12 11:58:49 +0100

    Load pattern files and test with marker number 5

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const path = require('path')
 app.get('/',function(req,res){
     app.use('/',express.static(path.join(__dirname,'/assets')))
     res.status(200).sendFile(path.join(__dirname,'/assets/html/index.html'))
-    console.log('Connected')
+    console.log('Connected to the webapp !')
+})
+
+app.get('/assets/pattern/:pattern', function(req,res){
+    const pattern = req.params.pattern
+    res.status(200).sendFile(path.join(__dirname + "/assets/pattern/" + pattern))
+    console.log('Pattern ' + pattern + ' is charged !')
 })
 
 console.log('Server launched !')
